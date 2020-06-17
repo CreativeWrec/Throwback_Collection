ce7c6e66afd5b4e0cef380b65ce7771aac3927c4
Kevin Harvey Jr <lordty@gmail.com>
2020-06-17 00:04:50 -0500

    Initial_Commit

---
+++ Throwback_Collection/Repository/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Data
+{
+   public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+
+}
